6a0d879e6502bd6d33805b6388e2896eb0e64554
Quincy <quincy@games.com>
2025-05-29 14:19:00 +0800

    feat: 优化主文件和样式设置
    - 更新 main.js 文件，调整导入顺序，保持一致性
    - 移除 base.css 中的全局颜色变量定义，转移至 main.css
    - 在 main.css 中添加全局颜色变量，优化样式管理

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import './assets/main.css'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+import "./assets/main.css"
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-import App from './App.vue'
-import router from './router/index'
+import App from "./App.vue"
+import router from "./router/index"
 
 const app = createApp(App)
 
@@ -14,4 +13,4 @@ app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
 
-app.mount('#app')
+app.mount("#app")
